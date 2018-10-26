e21fa37231f1aeb1cbd6845aa7557aea39282502
Toshiaki Seino <st12318@gmail.com>
2018-10-26 23:38:13 +0900

    Syntaxを修正した（つもり

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 crontab -l > /tmp/crontab.tmp
 echo "@reboot /home/pi/tsunagaru-kun-device/run.sh" >> /tmp/crontab.tmp
 
-if crontab -u pi /tmp/crontab.tmp then
+if [crontab -u pi /tmp/crontab.tmp]; then
 	echo "linstalling crontab is successfully"
 else
 	echo "installing crontab is failed"
